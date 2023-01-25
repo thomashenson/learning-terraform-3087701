9a91d25bcf8451e67cbed92fcec9a5add300a8da
Thomas Henson <thomasjameshenson@gmail.com>
2023-01-25 15:00:58 +0000

    Fixed vpc subnets to use variables

--- modules/blog/main.tf
+++ modules/blog/main.tf
@@ -21,7 +21,11 @@ module "blog_vpc" {
   cidr = "${var.environment.network_prefix}.0.0/16"
 
   azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  public_subnets  = [
+    "${var.environment.network_prefix}.101.0/24",
+    "${var.environment.network_prefix}.102.0/24",
+    "${var.environment.network_prefix}.103.0/24"
+  ]
 
   enable_nat_gateway = true
 
